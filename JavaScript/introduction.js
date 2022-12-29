15b32832aa536479ea8c550ddec5ac9e8e080a9d
Rajender Reddy B <79013189+Pekka1721@users.noreply.github.com>
2022-12-29 21:26:26 +0530

    JavaScript Introduction

---
+++ JavaScript/introduction.js
@@ -0,0 +1,14 @@
+"String"
+typeof "String"  //string type in console
+//data types in javascript
+/*
+1.String
+2.Number
+3.Boolean
+4.Undefined
+Use typeof to check datatype
+Declare variables with "let" key word
+Name variables with camelCase
+ */
+alert("Hello World");
+console.log("HelloWorld")
