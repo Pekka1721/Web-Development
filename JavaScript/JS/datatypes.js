7b2da410127d69789ed975fa2a465f2d0f069b6e
Rajender Reddy B <79013189+Pekka1721@users.noreply.github.com>
2023-01-02 20:20:47 +0530

    Add files via upload

---
+++ JavaScript/JS/datatypes.js
@@ -0,0 +1,10 @@
+
+
+//Datatypes
+/* Primitive Data types */
+let data =8         //Number
+let user = "Pekka"  //String
+
+let firstName = "Mini"
+let lastName = "Pekka"
+console.log(firstName+" "+lastName) //String Concatination
---
+++ JavaScript/JS/relationsOperator.js
@@ -0,0 +1,29 @@
+//<,>,==,!=,
+
+console.log(2>6)
+console.log(8>4)
+
+let x = 6
+let y = 8
+let data = x>=y
+console.log(data)
+
+x = "Pen"
+y = "Book"
+
+data =x>y
+console.log(data)
+
+x ='2'
+y =1
+data = x>y
+console.log(data)
+
+x = 3
+y =3
+console.log(x==y)
+
+x = "3"
+y = 3
+console.log(x==y)
+console.log(x===y)
